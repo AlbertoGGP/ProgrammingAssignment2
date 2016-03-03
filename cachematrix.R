e4145dc028be236afe0f19af73455acb21957ebc
AlbertoGGP <alberto.ggp@gmail.com>
2016-03-03 17:33:57 +0100

    Lexical Scoping exercise
    This is my solution for Programming Assignment 2 of Coursera course "R programming"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse, and have the required 
+## functions to store and recover the original matrix, or its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+	set <- function (y)
+	{
+		x <<- y
+		inv <<- NULL
+	}
+	
+	get <- function() x
+	setinv <- function(solve) inv <<- solve
+	getinv <- function () inv
+	list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function uses the object created with makeCacheMatrix function, and have two objetives:
+##   1. Verify if the original matrix have inverse. If it haven't, the function stops and show a message.
+##   2. If the original matrix have inverse, returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ## A matrix have inverse if it checks two conditions:
+	##  1. It is a square matrix (number of columns = number of rows)
+	##  2. Its determinant is different of 0
+	mtx <- x$get()
+	if (ncol(mtx) != nrow(mtx))
+		return(print("This matrix haven't inverse"))
+	else if (det(mtx)==0)
+		return(print("This matrix haven't inverse"))
+	
+	# If x matrix have inverse, the function calculates it
+	inv <- x$getinv()
+	if (!is.null(inv))
+	{
+		message("getting cache data")
+		return (inv)
+	}
+	
+	data <- x$get()
+	inv <- solve(data)
+	x$setinv(inv)
+	inv
 }
